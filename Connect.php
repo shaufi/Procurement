a8914478ae572c31db5df6216f011f98d5b1c213
shaufi <imanshaufi14@gmail.com>
2015-10-20 09:51:53 +0700

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,13 +1,10 @@
 <?php
-
-$dbhost = "localhost";
+$dbhost = "127.0.0.1";
 $dbuser = "root";
 $password = "";
-$dbname = "data rfq";
+$dbname = "data_rfq";
  		
 $con = mysql_connect($dbhost, $dbuser, $password) or die
 ("Koneksi gagal");
-
 mysql_select_db($dbname, $con);
-
 ?>
