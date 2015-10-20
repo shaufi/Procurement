c200c8b627750255015cc295d3c8ee2a851cf1a9
shaufi <imanshaufi14@gmail.com>
2015-10-20 09:51:17 +0700

    Update Insert.php

--- Insert.php
+++ Insert.php
@@ -1,36 +1,23 @@
 <?php
-
+	
+	include ('config.php');
  	if(isset($_POST["add"]))
  	{
- 		include('Connect.php');
-
- 		$ID = $_POST['ID'];
-		$Product = $_POST['Product']; 		
-		$Quantity = $_POST['Quantity'];
-		$Unit_Price = $_POST['Unit_Price'];
-		$Taxes = $_POST['Taxes'];
-		$Subtotal = $_POST['Subtotal'];
-
- 		$sql = mysql_query("INSERT INTO rfq
- 		VALUES ('$ID','$Product','$Quantity','
- 			$Unit_Price','$Taxes','$Subtotal')",$con) or die (mysql_error());			
-
+ 		include('config.php')
+ 		$ID = $_POST['id'];
+		$Product = $_POST['product']; 		
+		$Quantity = $_POST['quantity'];
+		$Unit_Price = $_POST['unit price'];
+		$Taxes = $_POST['taxes'];
+		$Subtotal = $_POST['subtotal'];
+ 		
+ 		$sql = echo "INSERT INTO rfq(id, product, quantity, unit_price, taxes, subtotal)
+ 		VALUES ($ID,'$Product',$Quantity,
+ 			$Unit_Price,$Taxes,$Subtotal)",$con) or die (mysql_error());			
+		$variabelapaaja = "sampai sini";
+		echo $variabelapaaja;
 		if($sql){
-		
-		echo 'Data berhasil di tambahkan! ';		//Pesan jika proses tambah sukses
-		echo '<a href="insert.php">Kembali</a>';	//membuat Link untuk kembali ke halaman tambah
-		
-	}else{
-		
-		echo 'Gagal menambahkan data! ';		//Pesan jika proses tambah gagal
-		echo '<a href="insert.php">Kembali</a>';	//membuat Link untuk kembali ke halaman tambah
-		
-	}
- 
-}else{	//jika tidak terdeteksi tombol tambah di klik
- 
-	//redirect atau dikembalikan ke halaman tambah
-	echo '<script>window.history.back()</script>';
- 
+ 			header("location: ")
+		}else{
 }
 ?>
